a8fa5a6689b852775115b8986011fe9e73b78785
WhimsyWretchWoods <riko.e610@gmail.com>
2025-06-18 13:10:23 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/quix/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.quix
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.quix.ui.theme.QuixTheme
+
+class MainActivity : ComponentActivity() {
+
+  @OptIn(ExperimentalMaterial3Api::class)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+
+    setContent {
+      QuixTheme {
+        val navController = rememberNavController()
+        Scaffold(
+        ) { innerPadding ->
+          Surface(
+            modifier = Modifier
+              .fillMaxSize()
+              .padding(innerPadding)
+          ) {
+            StoragePermissionCheck {
+              Navigation(navController = navController)
+            }
+          }
+        }
+      }
+    }
+  }
+}
