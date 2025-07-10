151fa16bbb64cae4506d97816440710fe751c1b4
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-10 15:05:12 +0700

    Delete app/src/main/java/com/quix/data directory

--- app/src/main/java/com/quix/data/Folder.kt
+++
@@ -1,9 +0,0 @@
-package com.quix.data
-
-import android.net.Uri
-
-data class FolderData(
-    val name: String,
-    val thumbnailUri: Uri,
-    val bucketId: String
-)
--- app/src/main/java/com/quix/data/Image.kt
+++
@@ -1,7 +0,0 @@
-package com.quix.data
-
-import android.net.Uri
-
-data class ImageData(
-    val uri: Uri
-)
